280c9c45d1297a46fafd09544a620e6122d07fb1
Román Díaz León <roman.diaz.leon@gmail.com>
2013-04-12 17:19:28 +0100

    Connecting twitter

--- src/com/yamba/StatusActivity.java
+++ src/com/yamba/StatusActivity.java
@@ -1,5 +1,7 @@
 package com.yamba;
 
+import winterwell.jtwitter.Twitter;
+import winterwell.jtwitter.TwitterException;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,6 +17,7 @@ public class StatusActivity extends Activity implements OnClickListener {
 	private static final String TAG = "Status Activit";
 	private Button updateButton;
 	private EditText editText;
+	private Twitter twitter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,10 +26,19 @@ public class StatusActivity extends Activity implements OnClickListener {
         updateButton = (Button) findViewById(R.id.buttonUpdate);
         updateButton.setOnClickListener((android.view.View.OnClickListener) this);
         editText = (EditText) findViewById(R.id.editText);
+        twitter = new Twitter("student", "password");
+        twitter.setAPIRootUrl("http://yamba.marakana.com/api");
     }
 
 	public void onClick(View arg0) {
-		Log.d(TAG, "onClick: " + editText.getText().toString());
+		try {
+			twitter.setStatus(editText.getText().toString());
+			Log.d(TAG, "onClick: " + editText.getText().toString());
+		}
+		catch (TwitterException e) {
+			Log.e(TAG, e.toString());
+			e.printStackTrace();
+		}
 	}
 
 }
