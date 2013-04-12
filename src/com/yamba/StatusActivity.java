5874be9e1cf55e6b1884cc92dcafcb1e8345532e
Román Díaz León <roman.diaz.leon@gmail.com>
2013-04-12 17:50:32 +0100

    Asynchronous Threads

--- src/com/yamba/StatusActivity.java
+++ src/com/yamba/StatusActivity.java
@@ -3,12 +3,14 @@ package com.yamba;
 import winterwell.jtwitter.Twitter;
 import winterwell.jtwitter.TwitterException;
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 
@@ -18,6 +20,32 @@ public class StatusActivity extends Activity implements OnClickListener {
 	private Button updateButton;
 	private EditText editText;
 	private Twitter twitter;
+	
+	private class PostToTwitter extends AsyncTask<String, Integer, String> {
+
+		@Override
+		protected String doInBackground(String... params) {
+			try {
+				Twitter.Status status = twitter.updateStatus(params[0]);
+				return status.text;
+			}
+			catch (TwitterException e) {
+				Log.e(TAG, e.toString());
+				e.printStackTrace();
+				return "Failed to post";
+			}
+		}
+		
+		@Override
+		protected void onProgressUpdate(Integer... values) {
+			super.onProgressUpdate(values);
+		}
+		
+		@Override
+		protected void onPostExecute(String result) {
+			Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+		}
+	}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,14 +59,8 @@ public class StatusActivity extends Activity implements OnClickListener {
     }
 
 	public void onClick(View arg0) {
-		try {
-			twitter.setStatus(editText.getText().toString());
-			Log.d(TAG, "onClick: " + editText.getText().toString());
-		}
-		catch (TwitterException e) {
-			Log.e(TAG, e.toString());
-			e.printStackTrace();
-		}
+		String status = editText.getText().toString();
+		new PostToTwitter().execute(status);
 	}
 
 }
